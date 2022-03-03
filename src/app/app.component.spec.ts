40ee87c8db7198ff29dd27cbd39386b154de1551
Mike Jancar <mikejancar@gmail.com>
2022-03-02 19:24:10 -0500

    feat: setup linting, testing and build
     - Added Jest and ESLint
     - Removed Jasmine and Karma
     - Added new build action

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,22 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { createSession } from './app.actions';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  const mockStore: any = {
+    dispatch: jest.fn()
+  };
+
+  let component: AppComponent;
+
+  beforeEach(() => {
+    component = new AppComponent(mockStore);
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'octodash'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('octodash');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('octodash app is running!');
+  describe('on initialization', () => {
+    test('it should create a session', () => {
+      component.ngOnInit();
+      expect(mockStore.dispatch.mock.calls.length).toBe(1);
+      expect(mockStore.dispatch.mock.calls[0][0]).toEqual(createSession());
+    });
   });
 });
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   clientId: string = '';
-
-  constructor() { }
-
-  ngOnInit(): void { }
 }
