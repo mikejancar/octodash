d478dafb9f349a011bffeed99d3517ebf33f6444
Mike Jancar <mikejancar@gmail.com>
2022-03-13 17:01:55 -0400

    bug: repair broken test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,38 @@
 import { fakeAsync, flush } from '@angular/core/testing';
 import { BehaviorSubject } from 'rxjs';
 
-import { createSession } from './app.actions';
+import { createSession, restoreGithubToken } from './app.actions';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   const mockToken = new BehaviorSubject('');
-  const mockStore: any = {
-    pipe: () => mockToken
-  };
+  let mockStore: any;
   let mockRouter: any;
 
   let component: AppComponent;
 
   beforeEach(() => {
     mockRouter = {
-      navigate: jest.fn()
+      navigate: jest.fn(),
     };
-    mockStore.dispatch = jest.fn();
+    mockStore = {
+      dispatch: jest.fn(),
+      pipe: () => mockToken,
+    };
     component = new AppComponent(mockStore, mockRouter);
   });
 
   describe('on initialization', () => {
+    test('it should restore the access token', () => {
+      component.ngOnInit();
+      expect(mockStore.dispatch.mock.calls.length).toBe(2);
+      expect(mockStore.dispatch.mock.calls[0][0]).toEqual(restoreGithubToken());
+    });
+
     test('it should create a session', () => {
       component.ngOnInit();
-      expect(mockStore.dispatch.mock.calls.length).toBe(1);
-      expect(mockStore.dispatch.mock.calls[0][0]).toEqual(createSession());
+      expect(mockStore.dispatch.mock.calls.length).toBe(2);
+      expect(mockStore.dispatch.mock.calls[1][0]).toEqual(createSession());
     });
 
     test('when no token is present, it should do nothing', fakeAsync(() => {
